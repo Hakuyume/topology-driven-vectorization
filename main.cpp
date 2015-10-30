0be2042945407716b3c81a337d2dd8f2a61f30c9
Toru Ogawa <Hakuyume@users.noreply.github.com>
2015-10-30 09:01:27 +0900

    関数定義を後ろに

--- main.cpp
+++ main.cpp
@@ -8,39 +8,8 @@ constexpr double epsCoeff = 0.1;
 constexpr double deltaT = 0.1;
 constexpr double movingLimit = 0.01;
 
-pixel::PixelSet mat2PixelSet(const cv::Mat &src)
-{
-  cv::Mat gradX, gradY;
-  cv::Sobel(src, gradX, CV_64F, 1, 0, 3);
-  cv::Sobel(src, gradY, CV_64F, 0, 1, 3);
-
-  cv::Mat gradMag;
-  cv::magnitude(gradX, gradY, gradMag);
-  double gradMax;
-  cv::minMaxLoc(gradMag, NULL, &gradMax, NULL, NULL);
-
-  const auto eps = gradMax * epsCoeff;
-  std::vector<cv::Point> movingIndexes;
-  cv::findNonZero(gradMag > eps, movingIndexes);
-
-  std::vector<pixel::Pixel> pixels;
-  for (const auto &index : movingIndexes)
-    pixels.push_back(
-        pixel::Pixel(
-            pixel::Vector(index.x, index.y),
-            pixel::Vector(gradX.at<double>(index), gradY.at<double>(index)) * deltaT));
-
-  return pixel::PixelSet(pixels);
-}
-
-cv::Mat pixelSet2Mat(const cv::Size &size, const pixel::PixelSet &pixelSet)
-{
-  cv::Mat mat{size, CV_8U, cv::Scalar(0)};
-  for (const auto &p : pixelSet.allPixels())
-    if (not p.isActive())
-      mat.at<uchar>(p.pos()(1), p.pos()(0)) = 255;
-  return mat;
-}
+pixel::PixelSet mat2PixelSet(const cv::Mat &src);
+cv::Mat pixelSet2Mat(const cv::Size &size, const pixel::PixelSet &pixelSet);
 
 int main(int argc, char *argv[])
 {
@@ -73,4 +42,38 @@ int main(int argc, char *argv[])
   }
 
   return 0;
+}
+
+pixel::PixelSet mat2PixelSet(const cv::Mat &src)
+{
+  cv::Mat gradX, gradY;
+  cv::Sobel(src, gradX, CV_64F, 1, 0, 3);
+  cv::Sobel(src, gradY, CV_64F, 0, 1, 3);
+
+  cv::Mat gradMag;
+  cv::magnitude(gradX, gradY, gradMag);
+  double gradMax;
+  cv::minMaxLoc(gradMag, NULL, &gradMax, NULL, NULL);
+
+  const auto eps = gradMax * epsCoeff;
+  std::vector<cv::Point> movingIndexes;
+  cv::findNonZero(gradMag > eps, movingIndexes);
+
+  std::vector<pixel::Pixel> pixels;
+  for (const auto &index : movingIndexes)
+    pixels.push_back(
+        pixel::Pixel(
+            pixel::Vector(index.x, index.y),
+            pixel::Vector(gradX.at<double>(index), gradY.at<double>(index)) * deltaT));
+
+  return pixel::PixelSet(pixels);
+}
+
+cv::Mat pixelSet2Mat(const cv::Size &size, const pixel::PixelSet &pixelSet)
+{
+  cv::Mat mat{size, CV_8U, cv::Scalar(0)};
+  for (const auto &p : pixelSet.allPixels())
+    if (not p.isActive())
+      mat.at<uchar>(p.pos()(1), p.pos()(0)) = 255;
+  return mat;
 }
